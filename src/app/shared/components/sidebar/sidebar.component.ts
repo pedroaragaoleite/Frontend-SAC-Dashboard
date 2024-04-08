4ce6d1c9287ee8e0429b2612cecd54b4b04a4533
Pedro Aragão <pedro.aragao.leite@gmail.com>
2024-04-08 15:19:47 +0200

    add responsive css

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -2,23 +2,33 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth/auth.service';
 import { DashboardModeService } from '../../../core/services/dashboard/dashboard-mode.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faInbox } from '@fortawesome/free-solid-svg-icons';
+import { initFlowbite } from 'flowbite';
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent implements OnInit {
   dashboardMode: "admin" | "supervisor" | "inbound" | "outbond";
 
+  faInbox = faInbox;
+  data: any;
+  name: any;
+
   constructor(private router: Router, private authService: AuthService, private dbService: DashboardModeService) {
     this.dashboardMode = this.dbService.getDashboardMode();
+    this.data = JSON.parse(localStorage.getItem('user') || 'null');
+    this.name = this.data.user.name;
    }
 
   ngOnInit(): void {
-
+    initFlowbite();
   }
 
   signOut(): void {
